d812726143bdbff2e961710fca012202014eb6e9
Dimitry <admin@ebstudy.net>
2012-01-15 21:09:43 -0500

    Workable. Download/mp3 encoding works.
    Signed-off-by: Dimitry <admin@ebstudy.net>

--- Youtube Downloader/src/main.java
+++ Youtube Downloader/src/main.java
@@ -5,24 +5,55 @@ import java.util.Scanner;
 public class main {
 
 	public static void main(String[] args) throws Exception {
+		String oursite = GetURL();
+		getPageSource(oursite);
+		String thename = getFileName();
+		DownloadVideo(oursite, thename);
+		ConvertVideoToMp3();
+
+	}
+
+	public static String GetURL() {
+
 		Scanner scan = new Scanner(System.in);
-		final youtubeInterface ninja = new youtubeInterface();
 
 		System.out
 				.println("Go ahead and enter in URL you want to download the youtube video of: ");
 
 		String URL = scan.nextLine(); // input url
+
 		if (!URL.startsWith("http://"))
 			URL = "http://" + URL;
 		if (!URL.startsWith("http://www."))
 			URL = "http://www." + URL;
-		System.out.println("What should I save the file as? ");
-		String FileName = scan.nextLine();
-		String downloadedURL = youtubeInterface.downloadWebpage(URL); // download http
-		String youtubeURL = youtubeInterface.FindYoutubeVideo(downloadedURL); // scan
+		return URL;
 
-		URL correct = new URL(youtubeURL);
-		File output = new File(FileName + ".flv");
+	}
+
+	public static String getFileName() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("What should I save the file as? ");
+		String filename = scan.nextLine();
+		return filename;
+	}
+
+	public static String getPageSource(String URL) throws IOException {
+
+		return youtubeInterface.downloadWebpage(URL);
+
+	}
+
+	public static void DownloadVideo(String URL, String FileName)
+			throws Exception {
+
+		final youtubeInterface ninja = new youtubeInterface();
+
+		String youtubeURL = youtubeInterface
+				.FindYoutubeVideo(getPageSource(URL)); // returns source of the
+														// specified url
+
+		URL correct = new URL(youtubeURL); // the url of the video file.
+		File output = new File("test" + ".flv");
 
 		final Downloader cat = new Downloader(correct, output);
 
@@ -32,12 +63,18 @@ public class main {
 				StringBuilder sb = new StringBuilder(200);
 
 				while (cat.isRunning() == true)
-					ninja.printProgBar(cat.getProgressPercent(), cat.getDownloadedLength(), cat.getLength());
+					ninja.printProgBar(cat.getProgressPercent(),
+							cat.getDownloadedLength(), cat.getLength());
 			}
 		};
 		Thread thr1 = new Thread(r);
 		thr1.start();
 		cat.run();
+		cat.waitUntilCompleted();
 	}
 
+	public static void ConvertVideoToMp3() {
+		convert2mp3 tomp4 = new convert2mp3();
+		tomp4.conversion("test.flv");
+	}
 }
--- Youtube Downloader/src/youtubeInterface.java
+++ Youtube Downloader/src/youtubeInterface.java
@@ -8,71 +8,71 @@ import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
 public class youtubeInterface {
-	
-		public static String FindYoutubeVideo(String input) {
-		//finds video
+
+	public static String FindYoutubeVideo(String input) {
+		// finds video
 		String ResultString = null;
 		try {
-			Pattern regex = Pattern.compile("http:\\\\/\\\\/.{0,100}generate_.{0,1000}\";");
+			Pattern regex = Pattern
+					.compile("http:\\\\/\\\\/.{0,100}generate_.{0,1000}\";");
 			Matcher regexMatcher = regex.matcher(input);
 			if (regexMatcher.find()) {
 				ResultString = regexMatcher.group();
-			} 
+			}
 		} catch (PatternSyntaxException ex) {
 			// Syntax error in the regular expression
 		}
 
-		//before we return, we find the REAL video...
-		//do some ninja decoding sh!t to find the actual video file.
+		// before we return, we find the REAL video...
+		// do some ninja decoding sh!t to find the actual video file.
 		ResultString = ResultString.substring(0, ResultString.length() - 2);
 		ResultString = ResultString.replace("\\/", "/");
-		ResultString = ResultString.replace("\\u0026","&");
+		ResultString = ResultString.replace("\\u0026", "&");
 		ResultString = ResultString.replace("generate_204", "videoplayback");
 		return ResultString;
 
 	}
 
+	// downloads html of a webpage.
+	public static String downloadWebpage(String link) throws IOException {
 
-		public static String downloadWebpage(String link) throws IOException {
+		URL url = new URL(link);
+		URLConnection urlConnection = url.openConnection();
+		urlConnection.setConnectTimeout(10000);
+		urlConnection.setReadTimeout(10000);
+		BufferedReader breader = new BufferedReader(new InputStreamReader(
+				urlConnection.getInputStream()));
 
-			URL url = new URL(link);
-			URLConnection urlConnection = url.openConnection();
-			urlConnection.setConnectTimeout(10000);
-			urlConnection.setReadTimeout(10000);
-			BufferedReader breader = new BufferedReader(new InputStreamReader(
-					urlConnection.getInputStream()));
+		StringBuilder stringBuilder = new StringBuilder();
 
-			StringBuilder stringBuilder = new StringBuilder();
+		String line;
+		while ((line = breader.readLine()) != null) {
+			stringBuilder.append(line);
+		}
 
-			String line;
-			while ((line = breader.readLine()) != null) {
-				stringBuilder.append(line);
-			}
-
-		
-			return stringBuilder.toString();
-		}	
-	public void printProgBar(int percent, int dld, int total){
-	    StringBuilder bar = new StringBuilder("[");
-	    StringBuilder bytes = new StringBuilder("50");
-	    for(int i = 0; i < 50; i++){
-	    
-	        if( i < (percent/2)){
-	            bar.append("=");
-	        }else if( i == (percent/2)){
-	            bar.append(">");
-	        }else{
-	            bar.append(" ");
-	        }
-	    }
-
-	    bar.append("] " + percent + "%   ");
-	    
-	    System.out.print("\r" + bar.toString() + dld/1024 + "kB of " + total/1024 + "kB");
-
-
+		return stringBuilder.toString();
 	}
 
+	// outputs progress bar + kB counter.
+	public void printProgBar(int percent, int dld, int total) {
+		StringBuilder bar = new StringBuilder("[");
+		StringBuilder bytes = new StringBuilder("50");
+		for (int i = 0; i < 50; i++) {
 
+			if (i < (percent / 2)) {
+				bar.append("=");
+			} else if (i == (percent / 2)) {
+				bar.append(">");
+			} else {
+				bar.append(" ");
+			}
+		}
+
+		bar.append("] " + percent + "%   ");
+
+		System.out.print("\r" + bar.toString() + dld / 1024 + "kB of " + total
+				/ 1024 + "kB");
+
+	}
 
 }
